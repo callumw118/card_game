c70feb40536012e1f8cba07aaddc71bcb383f866
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-24 14:56:03 +0100

    Adds a getCard method for player and test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class PlayerTest {
+
+    private Player player1;
+    private Player player2;
+    private Dealer dealer;
+    private Deck deck;
+
+    @Before
+    public void before(){
+        player1 = new Player();
+        player2 = new Player();
+        deck = new Deck();
+        deck.populateDeck();
+        dealer = new Dealer(deck);
+    }
+
+    @Test
+    public void handStartsEmpty(){
+        assertNull(player1.getCard());
+    }
 }
