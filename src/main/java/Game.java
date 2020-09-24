373ef689f343e3480219aa119e16cabefaed15d5
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-24 15:52:13 +0100

    can deal a card to each player and check who the winner is

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -21,4 +21,29 @@ public class Game {
     public Dealer getDealer() {
         return dealer;
     }
+
+    public Player checkWinner(){
+        Card player1Card = player1.getCard();
+        Card player2Card = player2.getCard();
+
+        if(player1Card.getValueOfEnum() > player2Card.getValueOfEnum()){
+            return player1;
+        }else {
+            return player2;
+        }
+    }
+
+    public String playGame(){
+        this.dealer.getDeck().populateDeck();
+        this.dealer.getDeck().shuffleDeck();
+        this.dealer.dealCard(player1);
+        this.dealer.dealCard(player2);
+        Player result = this.checkWinner();
+        if (result == player1){
+            return "Player 1 WINS!!!";
+        }
+        else{
+            return "Player 2 WINS!!!";
+        }
+    }
 }
--- src/test/java/DealerTest.java
+++ src/test/java/DealerTest.java
@@ -7,12 +7,13 @@ public class DealerTest {
 
     private Dealer dealer;
     private Deck deck;
+    private Player player;
 
     @Before
     public void before(){
         deck = new Deck();
-        deck.populateDeck();
         dealer = new Dealer(deck);
+        player = new Player();
     }
 
     @Test
@@ -22,7 +23,16 @@ public class DealerTest {
 
     @Test
     public void dealerDeckIsPopulatedWith52Cards(){
+        deck.populateDeck();
         int deckNum = dealer.getDeck().getNumberOfCards();
         assertEquals(52, deckNum);
+    }
+
+    @Test
+    public void canDealCard(){
+        deck.populateDeck();
+        Card cardToRemove = deck.getCards().get(0);
+        dealer.dealCard(player);
+        assertEquals(cardToRemove, player.getCard());
     }
 }
